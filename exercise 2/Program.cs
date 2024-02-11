b00bf4f5aa3ca37c8d23008f9aa227a2e54f61a9
GARCIA ALVAREZ SILVIA ANAHI <I23050337@monclova.tecnm.mx>
2024-02-10 22:09:16 -0600

    Agregar archivos de proyecto.

---
+++ exercise 2/Program.cs
@@ -0,0 +1,23 @@
+﻿//trapezoid area calculator
+try
+{
+
+	Console.WriteLine("Welcome to the trapezoid area calculator ");
+
+	Console.WriteLine("Please enter the major base: ");
+	double B_M = Convert.ToDouble(Console.ReadLine());
+
+	Console.WriteLine("Please enter the minor base: ");
+	double B_m = Convert.ToDouble(Console.ReadLine());
+
+	Console.WriteLine("Please enter the height: ");
+	double height = Convert.ToDouble(Console.ReadLine());
+
+	double area = ((B_M + B_m) * height) / 2;
+	Console.WriteLine("The area of your trapezoid is: " + area);
+}
+catch(FormatException)
+{
+	Console.WriteLine("The entry is not valid, be sure to enter numbers.  ");
+
+}
